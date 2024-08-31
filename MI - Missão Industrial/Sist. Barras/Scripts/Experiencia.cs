a68977f4e2c4dd433546321a7a57a441cfc6e40f
GabiFM010 <168764074+GabiFM010@users.noreply.github.com>
2024-08-31 12:10:53 -0300

    Update Experiencia.cs

--- MI - Missão Industrial/Sist. Barras/Scripts/Experiencia.cs
+++ MI - Missão Industrial/Sist. Barras/Scripts/Experiencia.cs
@@ -1,1 +1,26 @@
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
 
+    public class Barra_Experiencia: MonoBehaviour
+    {
+        public Slider slider;
+        // public Gradient gradiente;
+        public Image imagem;
+
+        public void SetMaxHealth(int health)
+        {
+            slider.maxValue = health;
+            slider.value = health;
+
+           // imagem.color = gradiente.Evaluate(1f);
+        }
+
+        public void SetHealth(int health)
+        {
+            slider.value = health;
+
+           // imagem.color = gradiente.Evaluate(slider.normalizedValue);
+        }
+    }
