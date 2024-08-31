518f112e6d79632386349efca7b7f447a880a879
GabiFM010 <168764074+GabiFM010@users.noreply.github.com>
2024-08-31 12:10:18 -0300

    Update Energia.cs

--- MI - Missão Industrial/Sist. Barras/Scripts/Energia.cs
+++ MI - Missão Industrial/Sist. Barras/Scripts/Energia.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class Energia_Bar : MonoBehaviour
+{
+    private Item item;
+    public Slider slider;
+    // public Gradient gradiente;
+    public Image imagem;
+    public int maxEnergia = 120;
+    
+    public int energiaAtual;
+
+    private void Start()
+    {
+        energiaAtual = maxEnergia;
+    }
+    public void SetMaxEnergia(float energia)
+    {
+        slider.maxValue = energia;
+        slider.value = energia;
+
+        // imagem.color = gradiente.Evaluate(1f);
+    }
+
+    public void SetEnergia(float energia)
+    {
+        slider.value = energia;
+        // imagem.color = gradiente.Evaluate(slider.normalizedValue);
+    }
+
+    public void GanhoEnergia(Item item)
+    {
+        SetEnergia(slider.value + item.item_energia);
+    }
+}
