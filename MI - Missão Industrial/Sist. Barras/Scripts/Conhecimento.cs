e3a276151ae50788759911318e74c305ee7b5584
GabiFM010 <168764074+GabiFM010@users.noreply.github.com>
2024-08-31 12:09:13 -0300

    Update Conhecimento.cs

--- MI - Missão Industrial/Sist. Barras/Scripts/Conhecimento.cs
+++ MI - Missão Industrial/Sist. Barras/Scripts/Conhecimento.cs
@@ -1,1 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Conhecimento : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradiente;
+    public Image progresso;  //fill
+
+
+    public void SetMaxHealth(int conhecimento)
+    {
+        slider.maxValue = conhecimento;
+        slider.value = conhecimento;
+        progresso.color = gradiente.Evaluate(1f);
+    }
+
+    public void SetHealth(int conhecimento)
+    {
+        slider.value = conhecimento;
+
+        progresso.color = gradiente.Evaluate(slider.normalizedValue);
+    }
+}
 
